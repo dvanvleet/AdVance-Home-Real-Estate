20ad4c1b3253faa00cf85c4da4892fcd9f2b8a31
dvanvleet <dvanvleet@unomaha.edu>
2022-10-30 14:23:20 -0500

    First Commit

---
+++ vancerealtor/vancerealtor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for vancerealtor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vancerealtor.settings')
+
+application = get_asgi_application()
